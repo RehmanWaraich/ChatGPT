35fce6d6074c56981c19b90cd221c39855cf3334
RehmanWaraich <rehmanwaraich2023@gmail.com>
2023-10-31 21:16:15 +0500

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from st_paywall import add_auth
+import openai
+import pandas as pd
+import numpy as np
+import math
+import random
 
 st.set_page_config(layout="wide")
 st.title("ChatGPT-3")
@@ -7,7 +12,42 @@ st.title("ChatGPT-3")
 add_auth(required=True)
 
 # only the user can see use the chagpt after the subscription 
+st.set_page_config(
+    page_title="ChatGPT",
+    page_icon="icon/favicon.png",
+)
 
-st.write(f"Subscription Status: {st.session_state.user_subscribed}")
-st.write("You all set the subscription!")
-st.write(f"But the way, your email is: {st.session_state.email}")
+st.markdown("<h1 style='text-align: center;'>ChatGPT</h1>", unsafe_allow_html=True)
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
